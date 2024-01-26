b1dc57d0bdf426160c0c3a9700fd9f9cd628acad
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-26 22:24:37 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,19 +7,7 @@ import seaborn as sns
 st.sidebar.title('WhatsApp Chat Analyzer')
 
 uploaded_file = st.sidebar.file_uploader("Choose a File")
-html_tag="""
-    <h5 style="color:black;text-align:left;">Incase of Clarifiction or doubts Contact me:-> 
-    <ul>
-      <li style="color:yellow;">
-      <a href="https://www.linkedin.com/in/neeraj-kumar-9a75811a2" style="color:#36AE7C;">Linkdin Profile</a>
-      </li>
-      <li style="color:yellow;">
-      <a href="https://github.com/neerajcodes888" style="color:#36AE7C;">Github Profile</a
-      </li>
-   </ul>
-    </h5> 
-      </div>
-    """
+st.markdown('''Uploaded File will be removed automatically after 1 hour of Upload''')
 
 if uploaded_file is None:
     
